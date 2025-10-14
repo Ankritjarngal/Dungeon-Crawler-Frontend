c1543854f0b796fe7ce14eb6696d5e8aab1066ac
ankrit <jarngalankrit@gmail.com>
2025-10-14 14:18:41 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
 
     const socketURL =
   process.env.NODE_ENV === "production"
-    ? "wss://artistic-gretal-ankritjarngal-9fa33e09.koyeb.app/ws"
+    ? "ws://artistic-gretal-ankritjarngal-9fa33e09.koyeb.app/ws"
     : "ws://localhost:8080/ws";
 
 
